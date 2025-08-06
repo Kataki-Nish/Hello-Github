3b26a6d89e88622ee75419d33fc9d99ef273236a
Kataki-Nish <kataki.nish@gmail.com>
2025-08-06 20:02:17 +0330

    Create my fisrt project in github
    add hello.py with hello to name

---
+++ hello.py
@@ -0,0 +1,15 @@
+#get name
+n=input("What is your name?")
+#show hello to name
+print("Hello",n)
+#say how to exit
+print("For exit write exit")
+#get exit
+while True:
+  wr=input().lower()
+  if wr=="exit":
+    break
+  else:
+    continue
+    
+    
